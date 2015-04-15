40bc88d69f928e725258d63cf46b5ca75e89e54c
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2015-04-15 11:00:09 -0300

    Working on
    Limpieza inicial de pantallas
    Armado de alta de factura

--- FEM-with-generator/app/scripts/app.js
+++ FEM-with-generator/app/scripts/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('femApp', ['ionic', 'femApp.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,6 +23,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
+  // -- HOME --
   .state('app', {
     url: "/app",
     abstract: true,
@@ -30,42 +31,116 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: "/search",
+  // -- TICKET --
+  .state('app.ticket-1', { // Seleccionar cliente
+    url: "/ticket-1",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/ticket-1.html",
+        controller: 'TicketCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-    url: "/browse",
+  .state('app.ticket-1-1', { // Agregar cliente
+    url: "/ticket-1-1",
+    templateUrl: "templates/ticket-1-1.html",
+    controller: 'TicketCtrl'
+  })
+
+  .state('app.ticket-2', { // Lista de conceptos
+    url: "/ticket-2",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/ticket-2.html",
+        controller: 'TicketCtrl'
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
+  .state('app.ticket-2-2', { // Agregar concepto
+    url: "/ticket-2-2",
+    templateUrl: "templates/ticket-2-2.html",
+    controller: 'TicketCtrl'
+  })
+
+  .state('app.ticket-3', { // Resumen
+    url: "/ticket-3",
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+        templateUrl: "templates/ticket-3.html",
+        controller: 'TicketCtrl'
       }
     }
-  });
+  })
+
+  .state('app.ticket-4', { // Resultado
+    url: "/ticket-4",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/ticket-4.html",
+        controller: 'TicketCtrl'
+      }
+    }
+  })
+
+  // -- HISTORY --
+  .state('app.history', {
+    url: "/history",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/history.html",
+        controller: 'HistoryCtrl'
+      }
+    },
+    cache:false // Will ask for objects again
+  })
+
+  .state('app.history.single', {
+    url: "/history/:ticketId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/history_ticket.html",
+        controller: 'HistoryTicketCtrl'
+      }
+    }
+  })
+
+  // -- CLIENTS --
+  .state('app.clients', {
+    url: "/clients",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/clients.html",
+        controller: 'ClientsCtrl'
+      }
+    },
+    cache:false // Will ask for objects again
+  })
+
+  .state('app.clientEdit', {
+    url: "/client/edit/:clientId/:name/:cuit/:email",
+    templateUrl: "templates/clientEdit.html",
+    controller: 'ClientEditCtrl'
+    }
+  })
+
+  // -- COFIGURATION --
+  .state('app.config', {
+    url: "/config",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/config.html",
+        controller: 'ConfigCtrl'
+      }
+    }
+  })
+
+// ----------------------
+
+; // Fin de states
+
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/ticket-1');
 });
